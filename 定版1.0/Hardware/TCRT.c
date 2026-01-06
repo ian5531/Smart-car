fe74d8d96926c497dadcfaa9bec8987622726dbd
于骞 <206684324@qq.com>
2026-01-06 15:18:13 +0800

    增加了超声波避障功能

---
+++ 定版1.0/Hardware/TCRT.c
@@ -0,0 +1,21 @@
+#include "stm32f10x.h"                  // Device header
+
+void TCRT_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Speed =GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+uint8_t TCRT_GetFlag(void)
+{
+	uint8_t TCRT_Flag;
+	
+	TCRT_Flag = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_8);
+	
+	return TCRT_Flag;
+}
