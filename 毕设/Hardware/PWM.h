a5c37d2b6cb67f8b58d63683b411fe13dbb9f479
于骞 <206684324@qq.com>
2026-01-06 15:07:26 +0800

    删除了文件夹

--- 毕设/Hardware/PWM.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PWM_H__
-#define __PWM_H__
-
-void PWM_Init(void);
-void PWM_SetCompare1(uint16_t Compare);
-void PWM_SetCompare2(uint16_t Compare);
-
-#endif
--- 毕设/Hardware/TCRT.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TCRT_H__
-#define __TCRT_H__
-
-void TCRT_Init(void);
-uint8_t TCRT_GetFlag(void);
-
-#endif
